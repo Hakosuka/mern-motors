61e22bb15623d40709f23e65236f8a36862294ca
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2020-09-01 16:51:39 +0100

    Created placeholder component classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
 	render() {
 		return(
-			<div className="container">
-				<h2>MERN Motors</h2>
-			</div>
+			<Router>
+				<Route path="/" exact component={VehiclesList} />
+				<Route path="/edit/:id" component={EditVehicle} />
+				<Route path="/create" component={AddVehicle} />
+				<div className="container">
+					<h2>MERN Motors</h2>
+				</div>
+			</Router>
 		);
 	}
 }
+export default App;

4652ec2694e9aed66367c93f85814be1dbd9d973
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2020-09-01 16:59:52 +0100

    Added basic layout and navigation.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AddVehicle from "./components/create-vehicle.component";
+import EditVehicle from "./components/edit-vehicle.component";
+import VehiclesList from "./components/vehicles-list.component";
+
+//import logo from "./logo.png";
+//import logo from "./logo.png";
+
 class App extends Component {
 	render() {
 		return(
 			<Router>
+				<div className="container">
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+						<Link to="/" className="navbar-brand">MERN Motors</Link>
+						<div className="collapse navbar-collapse">
+							<ul className="navbar-nav mr-auto">
+								<li className="navbar-item"><Link to="/" className="nav-link">All vehicles</Link></li>
+								<li className="navbar-item"><Link to="/create" className="nav-link">Add vehicle</Link></li>
+							</ul>
+						</div>
+					</nav>
+				<br/>
 				<Route path="/" exact component={VehiclesList} />
 				<Route path="/edit/:id" component={EditVehicle} />
 				<Route path="/create" component={AddVehicle} />
-				<div className="container">
-					<h2>MERN Motors</h2>
 				</div>
 			</Router>
 		);

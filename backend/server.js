776e6d63edd9ac86cf989f3642b65b95a469c67b
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2020-11-19 14:56:22 +0000

    Implemented MongoDB integration.

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 4000;
+const mongoose = require('mongoose');
+const PORT = 4008;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://127.0.0.1:27017/mern-motors', {useNewUrlParser: true});
+const connection = mongoose.connection;
+
+connection.once('open', function(){
+    console.log("MongoDB connection established successfully.");
+})
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });

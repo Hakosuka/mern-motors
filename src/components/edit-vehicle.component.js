07368067642ad4bd04837b732ea9882ba446cedd
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2020-11-19 17:22:27 +0000

    Vehicles now show up in VehiclesList
    The front-end can now access the database using Axios.

--- src/components/edit-vehicle.component.js
+++ src/components/edit-vehicle.component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class EditVehicle extends Component {
 	render(){
+		//TODO: Enable the user to edit the selected vehicle
 		return(
 			<div>
 				<p>Edit your vehicle here.</p>
--- src/components/vehicles-list.component.js
+++ src/components/vehicles-list.component.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+const Vehicle = props => (
+	<tr>
+		<td>{props.vehicle.vehicle_make}</td>
+		<td>{props.vehicle.vehicle_model}</td>
+		<td>{props.vehicle.vehicle_year}</td>
+		<td>{props.vehicle.vehicle_price}</td>
+		<td>{props.vehicle.vehicle_description}</td>
+		<td>{props.vehicle.vehicle_fuel}</td>
+		<td>
+			<Link to={"/edit/"+props.vehicle._id}>Edit</Link>
+		</td>
+	</tr>
+)
 export default class VehiclesList extends Component {
+	constructor(props){
+		super(props);
+		this.state = {vehicles: []};
+	}
+	componentDidMount(){
+		axios.get('http://localhost:4008/vehicles')
+			.then(response => {
+				this.setState({ vehicles: response.data});
+			}).catch(function(error){
+				console.log(error);
+			})
+	}
+	vehicleList() {
+		return this.state.vehicles.map(function(currentVehicle, listIndex){
+			return <Vehicle vehicle={currentVehicle} key={listIndex}/>;
+		})
+	}
 	render(){
 		return(
 			<div>
-				<p>Hello there. Welcome to the Vehicles List.</p>
+				<h3>Vehicles List</h3>
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th>Make</th>
+							<th>Model</th>
+							<th>Year</th>
+							<th>Price</th>
+							<th>Description</th>
+							<th>Fuel</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{ this.vehicleList() }
+					</tbody>
+				</table>
 			</div>
 		)
 	}
